80f61d58340765afc91c804c28d8262bb4485a6f
Mark Baumgartner <42626826+mabaumgartner@users.noreply.github.com>
2025-01-14 16:58:33 -0600

    lesson4-2 bugfix2

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.vpc_id.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   load_balancer_type = "application"
 
